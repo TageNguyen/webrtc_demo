f8acff0d67355b13746d2f3655882ab8cb1bee9c
Tage Nguyen <kimtrung153@gmail.com>
2024-05-06 11:56:21 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:webrtc_demo/service/signalling.dart';
+import 'screens/join_screen.dart';
+
+void main() {
+  // start videoCall app
+  runApp(VideoCallApp());
+}
+
+class VideoCallApp extends StatelessWidget {
+  VideoCallApp({super.key});
+
+  // signalling server url
+  final String websocketUrl = "SIGNALLING_SERVER_URL";
+
+  // generate callerID of local user
+  final String selfCallerID = Random().nextInt(999999).toString().padLeft(6, '0');
+
+  @override
+  Widget build(BuildContext context) {
+    // init signalling service
+    SignallingService.instance.init(
+      websocketUrl: websocketUrl,
+      selfCallerID: selfCallerID,
+    );
+
+    // return material app
+    return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: const ColorScheme.dark(),
+      ),
+      themeMode: ThemeMode.dark,
+      home: JoinScreen(selfCallerId: selfCallerID),
+    );
+  }
+}
